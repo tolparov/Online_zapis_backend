d9506bd7c8acd0fd65c875309afe6d0496c1d3f8
Alan <falase07@yandex.ru>
2024-04-27 19:15:30 +0300

    add customSecurityExpression

--- src/main/java/ru/alliedar/pokaznoi/web/security/expression/CustomSecurityExpression.java
+++ src/main/java/ru/alliedar/pokaznoi/web/security/expression/CustomSecurityExpression.java
@@ -1,45 +1,56 @@
-//package ru.alliedar.pokaznoi.web.security.expression;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Service;
-//import ru.alliedar.pokaznoi.domain.user.Role;
-//import ru.alliedar.pokaznoi.service.UserService;
-//import ru.alliedar.pokaznoi.web.security.JwtEntity;
-//
-//@Service("customSecurityExpression")
-//@RequiredArgsConstructor
-//public class CustomSecurityExpression {
-//
-//    private final UserService userService;
-//
-//    public boolean canAccessUser(Long id) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        JwtEntity user = (JwtEntity) authentication.getPrincipal();
-//        Long userId = user.getId();
-//        return userId.equals(id) || hasAnyRole(authentication, Role.ROLE_ADMIN);
-//    }
-//
-//    private boolean hasAnyRole (Authentication authentication, Role... roles) {
-//        for (Role role : roles) {
-//            SimpleGrantedAuthority authority = new SimpleGrantedAuthority((role.name()));
-//            if (authentication.getAuthorities().contains(authority)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    public boolean canAccessTask(Long taskId) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        JwtEntity user = (JwtEntity) authentication.getPrincipal();
-//        Long id = user.getId();
-//
-//        return userService.isTaskOwner(id, taskId);
-//    }
-//
-//}
+package ru.alliedar.pokaznoi.web.security.expression;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import ru.alliedar.pokaznoi.domain.user.Role;
+import ru.alliedar.pokaznoi.service.UserService;
+
+@Service("customSecurityExpression")
+@RequiredArgsConstructor
+public class CustomSecurityExpression {
+
+    private final UserService userService;
+	private final StringRedisTemplate stringRedisTemplate;
+
+    public boolean canAccessUser(Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Long userId = -1L;
+        String key = (String) authentication.getPrincipal();
+		String value = stringRedisTemplate.opsForValue().get(key);
+
+		if (value != null) {
+			userId = Long.parseLong(value);
+		}
+
+        return userId.equals(id) || hasAnyRole(authentication, Role.ROLE_ADMIN);
+    }
+
+    private boolean hasAnyRole (Authentication authentication, Role... roles) {
+        for (Role role : roles) {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority((role.name()));
+            if (authentication.getAuthorities().contains(authority)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean canAccessTask(Long taskId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		long userId = -1L;
+		String key = (String) authentication.getPrincipal();
+		String value = stringRedisTemplate.opsForValue().get(key);
+
+		if (value != null) {
+			userId = Long.parseLong(value);
+		}
+
+        return userService.isTaskOwner(userId, taskId);
+    }
+
+}
